7eb8e7e2d9d59423e57dc0af7064c4539fc82a44
LKLK <1047601623@qq.com>
2018-06-21 09:37:28 +0800

    未修改前

--- app/src/main/java/com/lg/happysmile/MainActivity.java
+++ app/src/main/java/com/lg/happysmile/MainActivity.java
@@ -11,12 +11,18 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.lg.adapter.MyViewPagerAdapter;
 import com.lg.fragment.CarefullyFragment;
 import com.lg.fragment.NewFragment;
@@ -26,14 +32,17 @@ import com.lg.fragment.ImageFragment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+public class MainActivity extends AppCompatActivity  {
     private TabLayout tab_layout;
-    private ViewPager viewPager;
+    private FrameLayout viewPager;
     private MyViewPagerAdapter view_adapter;
     private String[] titles = new String[]{"最新", "精选", "趣图", "段子"};
     private List<Fragment> fragments;
     private SharedPreferences sp;
     private SharedPreferences.Editor editor;
+    private BottomNavigationBar bottomNavigationBar;
+    protected Fragment currentFragment;
+    private FragmentManager fm;
     //检测网络连接状态
     private ConnectivityManager manager;
     //显示状态
@@ -55,24 +64,44 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     private void init() {
         sp = getSharedPreferences("Happy", MODE_PRIVATE);
         editor = sp.edit();
-        tab_layout = (TabLayout) findViewById(R.id.tab_layout);
-        viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        bottomNavigationBar=(BottomNavigationBar) findViewById(R.id.bottom_bar);
+        viewPager = (FrameLayout) findViewById(R.id.view_pager);
         fragments = new ArrayList<>();
-        //设置TabLayout标签的显示方式
-        tab_layout.setTabMode(TabLayout.MODE_FIXED);
-        //循环注入标签
-        for (String tab : titles) {
-            tab_layout.addTab(tab_layout.newTab().setText(tab));
-        }
-        //设置TabLayout点击事件
-        tab_layout.setOnTabSelectedListener(this);
         fragments.add(new NewFragment());
         fragments.add(new CarefullyFragment());
-        fragments.add(new ImageFragment());
         fragments.add(new JokeFragment());
-        view_adapter = new MyViewPagerAdapter(getSupportFragmentManager(), titles, fragments);
-        viewPager.setAdapter(view_adapter);
-        tab_layout.setupWithViewPager(viewPager);
+        fragments.add(new ImageFragment());
+
+        showFragment(new ImageFragment(),3);
+
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_book_white_24dp,titles[0])).setActiveColor(R.color.colorAccent)
+                .addItem(new BottomNavigationItem(R.drawable.ic_favorite_white_24dp,titles[1])).setActiveColor(R.color.colorAccent)
+                .addItem(new BottomNavigationItem(R.drawable.ic_home_white_24dp,titles[3]))
+                .setMode(BottomNavigationBar.MODE_SHIFTING)
+                .setFirstSelectedPosition(0)
+                .setBackgroundStyle( BottomNavigationBar.BACKGROUND_STYLE_STATIC)
+                .initialise();
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
+                Log.e("TAG", "onTabSelected: 111222" );
+                showFragment(fragments.get(position),position);
+            }
+
+            @Override
+            public void onTabUnselected(int position) {
+
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+
+            }
+        });
+
+//        viewPager.setAdapter(view_adapter);
+//        tab_layout.setupWithViewPager(viewPager);
     }
 
 
@@ -147,16 +176,34 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         startActivity(new Intent(this, SettingActivity.class));
     }
 
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-        viewPager.setCurrentItem(tab.getPosition());
-    }
 
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-    }
+    /**
+     * 显示Fragment
+     *
+     * @param fragment
+     */
+    protected void showFragment(Fragment fragment, int position) {
+        if (fm == null) {
+            fm = getSupportFragmentManager();
+        }
+        FragmentTransaction transaction = fm.beginTransaction();
+        //Fragment添加
+        if (!fragment.isAdded()) {
+//            fragment.setArguments(bundle);
+            transaction.add(R.id.view_pager, fragment, position + "");
+        }
+        if (currentFragment == null) {
+            currentFragment = fragment;
+        }
+        //通过tag进行过渡动画滑动判断
+        if (Integer.parseInt(currentFragment.getTag()) >= Integer.parseInt(fragment.getTag())) {
+            transaction.setCustomAnimations(R.anim.fragment_push_left_in, R.anim.fragment_push_right_out);
+        } else {
+            transaction.setCustomAnimations(R.anim.fragment_push_right_in, R.anim.fragment_push_left_out);
+        }
 
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
+        transaction.hide(currentFragment).show(fragment);
+        transaction.commit();
+        currentFragment = fragment;
     }
 }
